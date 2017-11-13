83f637007f4dc93da06b68c28ae658a79435d388
kevin <kevinverzuu@gmail.com>
2017-11-13 13:08:16 +0100

    yea gitignor werkt

---
+++ PJSDV/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: vrosty
+ *
+ * Created on 13 november 2017, 13:03
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("test");
+    return 0;
+}
+
